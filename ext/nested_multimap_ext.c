47569b9a5ca0a1222d7d5f5fc5c7af3c0ea56b27
Joshua Peek <josh@joshpeek.com>
2009-11-13 11:20:56 -0600

    undef [] before overriding it

--- ext/nested_multimap_ext.c
+++ ext/nested_multimap_ext.c
@@ -13,5 +13,6 @@ static VALUE rb_nested_multimap_aref(int argc, VALUE *argv, VALUE self)
 
 void Init_nested_multimap_ext() {
 	VALUE cNestedMultimap = rb_const_get(rb_cObject, rb_intern("NestedMultimap"));
+	rb_undef(cNestedMultimap, rb_intern("[]"));
 	rb_define_method(cNestedMultimap, "[]", rb_nested_multimap_aref, -1);
 }
